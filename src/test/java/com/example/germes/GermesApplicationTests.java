136d39ef1cf564f02895e09c945cf1dac9214f50
MatweyL <matweyka2003@mail.ru>
2022-05-04 21:53:37 +0300

    init project

---
+++ src/test/java/com/example/germes/GermesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.germes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GermesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
